63e68850264eb2f10e82e6824069899837520351
Po-Hsun Chen <octopopo@CharlottedeMacBook-Pro.local>
2015-12-15 20:14:53 +0800

    new scene

---
+++ Assets/Dreammancer/Script/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dreammancer
+{
+	public class Bullet : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void OnCollisionEnter2D(Collision2D other){
+			//Debug.Log ("HIT");
+			if (other.gameObject.tag == "Player") {
+				//Debug.Log ("HIT PLAYER");
+				//Debug.Log ("hey");
+				Destroy (this.gameObject);
+			}
+
+		}
+	}
+}
---
+++ Assets/Dreammancer/Script/ColorTrap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dreammancer
+{
+	
+	abstract public class ColorTrap : Trap
+	{
+		virtual public void OnParentDisappeared(){
+		}
+	}
+}
