88dac7f42b3041296f157896199c711de320beb8
許庭耀 <lulume0715@gmail.com>
2015-12-27 23:37:29 +0800

    Teach Stage

---
+++ Assets/Dreammancer/Script/UI/Instruction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Dreammancer
+{
+	public class Instruction : MonoBehaviour {
+
+		[SerializeField]
+		private string m_Message;
+		[SerializeField]
+		private Text m_Text;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		
+		void OnTriggerEnter2D(Collider2D other){
+			//Debug.Log ("HIT");
+			if (other.gameObject.tag == "Player") {
+				//Debug.Log ("HIT PLAYER");
+				//Debug.Log ("hey");
+				//Destroy (this.gameObject);
+				m_Text.text = m_Message;
+			}
+			
+		}
+	}
+}
