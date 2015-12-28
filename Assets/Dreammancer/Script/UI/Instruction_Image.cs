cb0447647205798c7b6a079de7b2575db0125604
許庭耀 <lulume0715@gmail.com>
2015-12-28 23:51:54 +0800

    New Billboard

---
+++ Assets/Dreammancer/Script/UI/Instruction_Image.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Dreammancer
+{
+	public class Instruction_Image : MonoBehaviour {
+
+
+		public GameObject billboard;
+		// Use this for initialization
+		void Start () {
+			billboard.SetActive(false);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			//billboard.SetActive(false);
+		}
+		void OnTriggerEnter2D(Collider2D other){
+			//Debug.Log ("HIT!!");
+			if (other.gameObject.tag == "Player") {
+				billboard.SetActive(true);
+			}
+			
+		}
+		void OnTriggerExit2D(Collider2D other){
+			if (other.gameObject.tag == "Player") {
+				billboard.SetActive(false);
+			}
+		}
+	}
+}
