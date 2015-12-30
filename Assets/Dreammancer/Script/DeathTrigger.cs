32f1d14b46d6fe795b77d00baca1da6d19afc240
williamd4112 <williamd4112@hotmail.com>
2015-12-30 15:22:45 +0800

    no message

---
+++ Assets/Dreammancer/Script/DeathTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dreammancer
+{
+    public class DeathTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] m_ListeningDeaths;
+
+        [SerializeField]
+        private Transform[] m_ToTriggers;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            bool result = true;
+            foreach(Transform trans in m_ListeningDeaths)
+            {
+                if (trans != null)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            if(result)
+            {
+                foreach (Transform trans in m_ToTriggers)
+                    trans.gameObject.SetActive(true);
+            }
+        }
+    }
+}
