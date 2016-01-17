d9463699f9121097f534af266f0c63fb11c933cd
drowsy810301 <lbj39919538@gmail.com>
2016-01-18 05:50:54 +0800

    dialog tmp

---
+++ Assets/Dialog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour {
+
+	[SerializeField] private Text txt; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
