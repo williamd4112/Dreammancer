9ca8f7516ef777e0d495503ec106fefcd3f38b30
許庭耀 <lulume0715@gmail.com>
2016-01-18 01:51:27 +0800

    switch scene & button

---
+++ Assets/Dreammancer/Script/Save/FinishLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Dreammancer{
+	public class FinishLevel : MonoBehaviour {
+
+		public string LevelName;
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public void OnTriggerEnter2D(Collider2D other){
+
+			Debug.Log("ok!!!");
+
+			if (other.GetComponent<DreammancerCharacter> () == null) {
+
+				Debug.Log("something");
+				return;
+			}
+			LevelManager.Instance.GotoNextLevel (LevelName);
+		}
+	}
+}
