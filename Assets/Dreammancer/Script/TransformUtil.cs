c821147566b49b64a3b20f46cdfe4dd57e400c80
unknown <williamd4112@hotmail.com>
2016-01-18 05:58:13 +0800

    Dynamic Scene: 1. Background Color 2. FixedPlatform

---
+++ Assets/Dreammancer/Script/TransformUtil.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dreammancer
+{
+	public class TransformUtil : MonoBehaviour {
+
+		public static void Shake(Vector3 pos, Vector3 pivot, Vector3 radius, float rate)
+		{
+
+		}
+	}
+}
