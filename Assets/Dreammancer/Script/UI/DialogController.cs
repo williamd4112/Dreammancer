38377a744e67a120ac77fae7148a85e4da57eb71
drowsy810301 <lbj39919538@gmail.com>
2016-01-19 01:17:47 +0800

    gg

--- Assets/Dreammancer/Script/UI/DialogController.cs
+++ Assets/Dreammancer/Script/UI/DialogController.cs
@@ -12,29 +12,33 @@ public class DialogController : MonoBehaviour {
 
 	private List<string> sentence = new List<string>();
 
+	private Dictionary<string, int> speaker = new Dictionary<string, int>();
 	private int x = 0;
 	private GameObject m_dialog;
 	private Image m_image;
 	private Text dialog_txt;
-
+	private string cur_speaker;
 	// Use this for initialization
 
 	void Awake(){
 		m_dialog = GameObject.FindGameObjectWithTag ("Dialog");
 		dialog_txt = m_dialog.GetComponentInChildren<Text>();
 		dialog_txt.text = "";
+		cur_speaker = "";
 	}
 	void Start () {
 		sentence.Clear ();
 		readTextFile ("Assets/Dreammancer/Dialog/" + select_diolag);
+		setPhoto ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Debug.Log (dialog_txt.text);
 		if (Input.GetMouseButtonDown (0) && x < sentence.Count && m_image.enabled) {
+			cur_speaker = getSpeaker(sentence[x-1]);
 			dialog_txt.text = sentence [x];
-			x = x + 1;
+			x = x + 2;
 			if(x >= sentence.Count){
 				dialog_txt.text = "";
 				m_image.enabled = false;
@@ -42,7 +46,6 @@ public class DialogController : MonoBehaviour {
 				Destroy (gameObject);
 			}
 		}
-
 	}
 	
 	void FixedUpdate(){
@@ -52,15 +55,22 @@ public class DialogController : MonoBehaviour {
 	void readTextFile(string file_path)
 	{
 		StreamReader inp_stm = new StreamReader(file_path);
-		
+		int i = 0;
 		while(!inp_stm.EndOfStream)
 		{
 			sentence.Add(inp_stm.ReadLine());
-			// Do Something with the input. 
-			Debug.Log(sentence);
+			if(i % 2 == 0 && sentence[i] != " "){
+				if(speaker.ContainsKey(sentence[i])){
+					speaker[sentence[i]] = speaker[sentence[i]] + 1;
+				}
+				else{
+					speaker.Add(sentence[i], 1);
+				}
+			}
+			i = i + 1;
 		}
-		
-		inp_stm.Close( );  
+		Debug.Log (speaker.Count + 100);
+		inp_stm.Close();  
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -70,8 +80,9 @@ public class DialogController : MonoBehaviour {
 			Time.timeScale = 0;
 			m_image = m_dialog.GetComponent<Image>();
 			m_image.enabled = true;
-			dialog_txt.text = sentence [x];
-			x = x + 1;
+			dialog_txt.text = sentence [1];
+			cur_speaker = getSpeaker(sentence[0]);
+			x = 3;
 		}
 		
 	}
@@ -81,4 +92,15 @@ public class DialogController : MonoBehaviour {
 		
 	}
 
+	private string getSpeaker(string raw){
+		string sp;
+		sp = raw.Replace ("[","");
+		sp = sp.Replace ("]","");
+		return sp;
+	}
+
+	private void setPhoto(){
+
+
+	}
 }
