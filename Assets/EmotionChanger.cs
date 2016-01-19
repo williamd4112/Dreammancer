8eca8f6a6e2c038753cd9594368859954d9c0374
williamd4112 <williamd4112@hotmail.com>
2016-01-20 04:21:41 +0800

    no message

---
+++ Assets/EmotionChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dreammancer
+{
+    public class EmotionChanger : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
